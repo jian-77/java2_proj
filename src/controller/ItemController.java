eaf23e47924a41f1d26ddbdf9ebfddcc708780bd
ryan <2716564567@qq.com>
2024-05-31 20:36:57 +0800

    2 commit

--- src/controller/ItemController.java
+++ src/controller/ItemController.java
@@ -10,22 +10,6 @@ public class ItemController {
 
     Connection conn=DatabaseConnection.getConn();
 
-    public void addItemType(Item item){
-        try{
-            String sql="insert into item(name,type,returnable) values (?,?,?) returning id";
-            PreparedStatement stmt= conn.prepareStatement(sql);
-            stmt.setString(1,item.getName());
-            stmt.setString(2, item.getType());
-            stmt.setBoolean(3, item.isReturnable());
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()){
-                item.setId(rs.getLong("id"));}
-            else{
-                System.out.println("no generate id");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+
 
 }
